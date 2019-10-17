1716266f9168709e239cdd0d912997c8999cef46
Katarzyna <katarzyna.panek111@gmail.com>
2019-10-17 21:39:51 +0200

    zad 2 poprawka 1.

--- src/Names.java
+++ src/Names.java
@@ -6,11 +6,12 @@ public class Names {
         Scanner input = new Scanner(System.in);
         String names[] = new String[5];
         int i = 0;
+        int lenght = 5;
         do {
             System.out.println("Podaj imię");
             names[i] = input.nextLine();
             i++;
-        } while (i < 5);
+        } while (i < lenght);
         input.close();
 
         i = 0;
@@ -18,7 +19,6 @@ public class Names {
             System.out.println(names[i]);
             i++;
         }
-        i = names.length;
         do {
             i--;
             System.out.println("Cześć " + names[i]);
